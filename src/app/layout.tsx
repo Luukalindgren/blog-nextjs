65a92193d5ef91fe50d37907a79da14e86fa90e8
Luuka Lindgren <luslin@utu.fi>
2023-05-24 16:05:39 +0300

    Added TODO list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,19 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 
+//TODO:
+// - Make backend for blog posts and book summaries (Supabase?)
+// - Make site fetch data from backend
+// - Add functionality to add new posts and book summaries
+// - Add account system (Register, log in, log out etc.)
+// - Add functionality to edit and delete posts and book summaries
+// - Add functionality to comment on posts and book summaries
+// - Make post adding, editing and deleting to be available only for logged in users
+// - Publish site to some hosting service (Azure or Vercel?)
+// - Finish README.md
+// - Add styling and maybe animations?
+
+
 export const metadata: Metadata = {
   title: "Luuka's blog",
   description: "Blog site hosted by Luuka Lindgren",
