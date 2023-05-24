84bb3a53be8ae74eb83c21fa4d5d6ed4ef9c3711
Luuka Lindgren <luslin@utu.fi>
2023-05-24 14:58:23 +0300

    Added header and footer to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
-import { Metadata } from 'next'
-import './globals.css'
+import { Metadata } from "next";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Luuka's blog",
-  description: 'Blog site hosted by Luuka Lindgren',
-}
+  description: "Blog site hosted by Luuka Lindgren",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer/>
+      </body>
     </html>
-  )
+  );
 }
