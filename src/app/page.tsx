6180b4cb6f0e5678e3b01b9b08fd18aa517b0a86
Luuka Lindgren <luslin@utu.fi>
2023-05-26 15:01:41 +0300

    Added date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
 import BlogPostCard from "@/components/BlogPostCard";
 import PostWidget from "@/components/PostWidget";
-import { supabase } from "./supabaseClient";
+import { supabase } from "./utils/supabaseClient";
+
+export const revalidate = 0;
 
 export default async function Home() {
   const { data: posts } = await supabase.from("blog_posts").select("*");
-  
+
   return (
     <main className="container min-h-screen px-10 mx-auto mb-8 font-mono">
       <h2 className="py-6 text-lg font-bold">Viikottaiset kuulumiset:</h2>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {!posts ? "" : posts.map((post, index) =>
-            BlogPostCard({
-              title: post.title,
-              desc: post.post,
-              index: index,
-            })
-          )}
+          {!posts
+            ? ""
+            : posts.map((post, index) =>
+                BlogPostCard({
+                  title: post.title,
+                  date: post.inserted_at,
+                  desc: post.post,
+                  index: index,
+                })
+              )}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -1,7 +1,11 @@
-export default function BlogPostCard(prop: { title: string, desc: string, index: number}) {
+export default function BlogPostCard(prop: { title: string, desc: string, index: number, date: string}) {
+
+  const formattedDate= prop.date.split("T")[0].split("-").reverse().join(".")
+  
     return (
-      <div className="p-4 my-2 text-white border-2 rounded-lg bg-black/40" key={prop.index}>
-        <h2 className="font-semibold">{prop.title}</h2>
+      <div className="p-4 my-2 border-2 rounded-lg bg-black/10" key={prop.index}>
+        <h2 className="font-semibold text-center">{prop.title}</h2>
+        <h3 className="mb-4 text-sm italic text-center">{formattedDate}</h3>
         <p className="">{prop.desc}</p>
       </div>
     );
