a1e5445956ab37f8f26e59ba4e503cee71cef99c
Luuka Lindgren <luslin@utu.fi>
2023-05-27 23:40:12 +0300

    Added text formatting, like line breaks etc.

--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -1,13 +1,21 @@
-export default function BlogPostCard(prop: { title: string, desc: string, index: number, date: string}) {
+export default function BlogPostCard(prop: {
+  title: string;
+  desc: string;
+  index: number;
+  date: string;
+}) {
+  const formattedDate = prop.date.split("T")[0].split("-").reverse().join(".");
 
-  const formattedDate= prop.date.split("T")[0].split("-").reverse().join(".")
-  
-    return (
-      <div className="p-4 my-2 border-2 rounded-lg bg-black/10" key={prop.index}>
-        <h2 className="font-semibold text-center">{prop.title}</h2>
-        <h3 className="mb-4 text-sm italic text-center">{formattedDate}</h3>
-        <p className="">{prop.desc}</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="p-4 my-4 rounded-lg shadow-lg bg-black/10" key={prop.index}>
+      <h2 className="font-semibold text-center">{prop.title}</h2>
+      <h3 className="mb-4 text-sm italic text-center">{formattedDate}</h3>
+      {prop.desc.split("\n").map((paragraph, index) => (
+        <p key={index}>
+          {paragraph}
+          <br />
+        </p>
+      ))}
+    </div>
+  );
+}
--- src/components/BookPostCard.tsx
+++ src/components/BookPostCard.tsx
@@ -1,9 +1,14 @@
 export default function BookPostCard(prop: { title: string, desc: string, index: number, author: string, rating: number, }) {
   return (
-    <div className="p-4 mt-2 border-2 rounded-lg bg-black/10" key={prop.index}>
+    <div className="p-4 my-4 rounded-lg shadow-lg bg-black/10" key={prop.index}>
       <h2 className="font-semibold text-center">{prop.title}</h2>
       <h3 className="mb-4 text-sm italic text-center">{prop.author}</h3>
-      <p className="">{prop.desc}</p>
+      {prop.desc.split("\n").map((paragraph, index) => (
+        <p key={index}>
+          {paragraph}
+          <br />
+        </p>
+      ))}
       <p className="mt-4 mb-2 font-semibold text-center">Arvosana: {prop.rating}/5</p>
     </div>
   );
