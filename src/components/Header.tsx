969ba5ffc2bd5cb562733be93c27b9a55019952c
Luuka Lindgren <luslin@utu.fi>
2023-05-27 23:41:06 +0300

    Changed navbar and added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,41 @@
 import React, { useContext } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <nav className="container px-10 mx-auto mb-8">
-      <div className="inline-block w-full py-8 border-b border-gray-800">
-        <div className="block md:float-left">
-          <Link href="/">
-            <h1 className="text-4xl font-bold text-white cursor-pointer">
-              Luukan Blogi
-            </h1>
-          </Link>
-        </div>
-        <div className="contents ">
-          <Link href="/account">
-            <span className="mt-2 ml-4 font-semibold text-white align-middle cursor-pointer md:float-right">
-              Käyttäjä
-            </span>
-          </Link>
-          <Link href="/books">
-            <span className="mt-2 ml-4 font-semibold text-white align-middle cursor-pointer md:float-right">
-              Kirjatiivistelmät
-            </span>
-          </Link>
-          <Link href="/">
-            <span className="mt-2 ml-4 font-semibold text-white align-middle cursor-pointer md:float-right">
-              Blogi
-            </span>
-          </Link>
-        </div>
+    <header className="flex justify-between px-10 py-5 space-x-2 font-bold text-white item-center">
+      <div className="flex items-center space-x-2 ">
+        <Link href="/">
+          <Image
+            src="/LL.png"
+            width={100}
+            height={100}
+            alt="Luuka Lindgren Logo"
+          />
+        </Link>
+          <h1 className="text-4xl transition duration-500 cursor-pointer hover:text-gray-100/80">
+            :n Blogi
+          </h1>
       </div>
-    </nav>
+
+      <div className="flex items-center py-8 space-x-2 md:space-x-6">
+        <Link href="/">
+          <span className="align-middle transition duration-500 cursor-pointer hover:text-gray-100/80">
+            Blogi
+          </span>
+        </Link>
+        <Link href="/books">
+          <span className="align-middle transition duration-500 cursor-pointer hover:text-gray-100/80">
+            Kirjatiivistelmät
+          </span>
+        </Link>
+        <Link href="/account">
+          <span className="align-middle transition duration-500 cursor-pointer hover:text-gray-100/80">
+            Käyttäjä
+          </span>
+        </Link>
+      </div>
+    </header>
   );
 }
