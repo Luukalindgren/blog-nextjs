b5bf4ea0bb18445dbaebc028e811cdcaa65d9d3e
Luuka Lindgren <luslin@utu.fi>
2023-05-27 23:41:14 +0300

    Update TODO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,12 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 
 //TODO:
-// - Make backend for blog posts and book summaries (Supabase?)
-// - Make site fetch data from backend
+// Done Make backend for blog posts and book summaries (Supabase?)
+// Done Make site fetch data from backend
+// Done Make posts appear logically, now they are not showing new lines etc.
+// - Make ratings appear as stars or some images
 // - Add functionality to add new posts and book summaries
+// - Add sorting system to book summaries (by rating, by date, alphabetically etc.)
 // - Add account system (Register, log in, log out etc.)
 // - Add functionality to edit and delete posts and book summaries
 // - Add functionality to comment on posts and book summaries
@@ -14,7 +17,6 @@ import "./globals.css";
 // - Publish site to some hosting service (Azure or Vercel?)
 // - Finish README.md
 // - Add styling and maybe animations?
-
 
 export const metadata: Metadata = {
   title: "Luuka's blog",
@@ -31,7 +33,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
