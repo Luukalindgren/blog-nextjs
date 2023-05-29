94518094a85e64e9568f8de20ea927ff79693035
Luuka Lindgren <luslin@utu.fi>
2023-05-29 13:18:01 +0300

    Site lang en -> fi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="fi">
       <body>
         <Header />
         {children}
