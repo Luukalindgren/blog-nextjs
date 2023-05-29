e6e5e684e5771af8e997f3ef4d5beb719f276a5f
Luuka Lindgren <luslin@utu.fi>
2023-05-29 13:18:41 +0300

    Changed the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,48 @@
-import BlogPostCard from "@/components/BlogPostCard";
-import PostWidget from "@/components/PostWidget";
-import { supabase } from "./utils/supabaseClient";
+import BlogPostPreview from "@/components/BlogPostPreview";
+import BookPostPreview from "@/components/BookPostPreview";
+import { supabase } from "./lib/supabaseClient";
 
 export const revalidate = 0;
 
 export default async function Home() {
   const { data: posts } = await supabase.from("blog_posts").select("*");
-
+  const { data: bookSummaries } = await supabase
+    .from("book_summaries")
+    .select("*");
   return (
-    <main className="container min-h-screen px-10 mx-auto mb-8 font-mono">
-      <h2 className="py-6 text-lg font-bold">Viikottaiset kuulumiset:</h2>
-      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-        <div className="col-span-1 lg:col-span-8">
-          {!posts
-            ? ""
-            : posts.map((post, index) =>
-                BlogPostCard({
-                  title: post.title,
-                  date: post.inserted_at,
-                  desc: post.post,
-                  index: index,
-                })
-              )}
+    <main className="container min-h-screen px-10 mx-auto font-mono text-center">
+      <div>
+        <h2 className="py-6 text-lg font-bold">Blogipostaukset:</h2>
+        <div className="grid grid-cols-1 gap-4 p-4 my-4 rounded-lg shadow-lg sm:grid-cols-2 lg:grid-cols-3 bg-black/10">
+          {!posts ? (
+            <p>Blogipostauksien haku epäonnistui!</p>
+          ) : (
+            posts.map((post) =>
+              BlogPostPreview({
+                title: post.title,
+                date: post.inserted_at,
+                index: post.id,
+              })
+            )
+          )}
         </div>
-        <div className="col-span-1 lg:col-span-4">
-          <div className="relative lg:sticky top-8">
-            <PostWidget />
-          </div>
+      </div>
+      <div>
+        <h2 className="py-6 text-lg font-bold">Kirjatiivistelmät:</h2>
+        <div className="grid grid-cols-1 gap-4 p-4 my-4 rounded-lg shadow-lg sm:grid-cols-2 lg:grid-cols-3 bg-black/10">
+          {!bookSummaries ? (
+            <p>Tiivistelmien haku epäonnistui!</p>
+          ) : (
+            bookSummaries.map((post) =>
+              BookPostPreview({
+                title: post.title,
+                author: post.author,
+                desc: post.summary,
+                index: post.id,
+                rating: post.rating,
+              })
+            )
+          )}
         </div>
       </div>
     </main>
