c98ccda08b0491c91c7901f5c5e14f566f1955c1
Luuka Lindgren <luslin@utu.fi>
2023-06-05 11:06:46 +0300

    Added file for different types

---
+++ src/components/types.ts
@@ -0,0 +1,14 @@
+export type Post = {
+    id: number;
+    title: string;
+    post: string;
+    inserted_at: string;
+  };
+  
+export type Book = {
+    id: number;
+    title: string;
+    author: string;
+    summary: string;
+    rating: number;
+  };
