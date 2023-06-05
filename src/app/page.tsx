5dfbbd207531bed35241e9df48ac9f003a449f76
Luuka Lindgren <luslin@utu.fi>
2023-06-05 11:07:54 +0300

    Added use hooks to fix render not mathing db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,48 +3,61 @@
 import BlogPostPreview from "@/components/BlogPostPreview";
 import BookPostPreview from "@/components/BookPostPreview";
 import { supabase } from "./supabaseClient";
+import { useEffect, useState } from "react";
+import { Book, Post } from "@/components/types";
 
 export const revalidate = 0;
 
 export default async function Home() {
-  const { data: posts } = await supabase.from("blog_posts").select("*");
-  const { data: bookSummaries } = await supabase
-    .from("book_summaries")
-    .select("*");
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [bookSummaries, setBookSummaries] = useState<Book[]>([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const { data } = await supabase.from("blog_posts").select("*");
+      if (!data) return;
+
+      setPosts(data as Post[]);
+    }
+    async function fetchBookSummaries() {
+      const { data } = await supabase.from("book_summaries").select("*");
+      if (!bookSummaries) return;
+      setBookSummaries(data as Book[]);
+    }
+    fetchPosts();
+    fetchBookSummaries();
+  }, []);
+
   return (
     <main className="container min-h-screen px-10 mx-auto font-mono text-center">
       <div>
         <h2 className="py-6 text-lg font-bold">Blogipostaukset:</h2>
         <div className="grid grid-cols-1 gap-4 p-4 my-4 rounded-lg shadow-lg sm:grid-cols-2 lg:grid-cols-3 bg-black/10">
-          {!posts ? (
-            <p>Blogipostauksien haku epäonnistui!</p>
-          ) : (
-            posts.map((post) =>
-              BlogPostPreview({
-                title: post.title,
-                date: post.inserted_at,
-                index: post.id,
-              })
-            )
-          )}
+          {!posts
+            ? ""
+            : posts.map((post) =>
+                BlogPostPreview({
+                  title: post.title,
+                  date: post.inserted_at,
+                  index: post.id,
+                })
+              )}
         </div>
       </div>
       <div>
         <h2 className="py-6 text-lg font-bold">Kirjatiivistelmät:</h2>
         <div className="grid grid-cols-1 gap-4 p-4 my-4 rounded-lg shadow-lg sm:grid-cols-2 lg:grid-cols-3 bg-black/10">
-          {!bookSummaries ? (
-            <p>Tiivistelmien haku epäonnistui!</p>
-          ) : (
-            bookSummaries.map((post) =>
-              BookPostPreview({
-                title: post.title,
-                author: post.author,
-                desc: post.summary,
-                index: post.id,
-                rating: post.rating,
-              })
-            )
-          )}
+          {!bookSummaries
+            ? ""
+            : bookSummaries.map((post) =>
+                BookPostPreview({
+                  title: post.title,
+                  author: post.author,
+                  desc: post.summary,
+                  index: post.id,
+                  rating: post.rating,
+                })
+              )}
         </div>
       </div>
     </main>
