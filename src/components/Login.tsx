d5eee92b048561a63db67977a966f54a0e6ab9f4
Luuka Lindgren <luslin@utu.fi>
2023-06-06 12:30:10 +0300

    Changed language and styling on buttons

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -68,22 +68,23 @@ export default function Login() {
         />
         <button
           className="px-16 py-4 transition duration-500 shadow-xl bg-black/20 rounded-xl hover:bg-black/40 active:scale-95"
-          onClick={handleSignUp}
-        >
-          Sign up
-        </button>
-        <button
-          className="px-16 py-4 transition duration-500 shadow-xl bg-black/20 rounded-xl hover:bg-black/40 active:scale-95"
           onClick={handleSignIn}
         >
-          Sign in
+          Kirjaudu sisään
         </button>
         <button
           className="px-16 py-4 transition duration-500 shadow-xl bg-black/20 rounded-xl hover:bg-black/40 active:scale-95"
           onClick={handleSignOut}
         >
-          Sign out
+          Kirjaudu ulos
         </button>
+        {!user && (
+        <button
+          className="w-1/2 col-span-2 px-16 py-4 m-auto transition duration-500 shadow-xl bg-black/20 rounded-xl hover:bg-black/40 active:scale-95"
+          onClick={handleSignUp}
+        >
+          Luo tili
+        </button>)}
       </div>
 
       {user ? (
