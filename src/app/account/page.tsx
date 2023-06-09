b1110e8f832214c1c46925d923dea079e16b174f
Luuka Lindgren <luslin@utu.fi>
2023-06-09 12:58:16 +0300

    Divided login form and account info from eachother

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import Login from "@/components/Login";
 import AccountInfo from "@/components/Account";
+import { useAuthContext } from "../context";
 
 export default function Account() {
+  const { user } = useAuthContext();
+
   return (
     <main className="container min-h-screen px-10 mx-auto mb-8 font-mono">
-      <h2 className="py-6 text-lg font-bold">Käyttäjän tiedot:</h2>
-      <Login />
-      <AccountInfo />
+      {user ? <AccountInfo /> : <Login />}
     </main>
   );
 }
