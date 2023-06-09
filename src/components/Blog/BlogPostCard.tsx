44e1fddc9ba04dca1bbf34db6599445ceca31a85
Luuka Lindgren <luslin@utu.fi>
2023-06-09 14:41:54 +0300

    Added post/summary removing, only for logged in admin

--- src/components/Blog/BlogPostCard.tsx
+++ src/components/Blog/BlogPostCard.tsx
@@ -1,13 +1,49 @@
+"use client";
+
+import { useAuthContext } from "@/app/context";
+import { supabase } from "@/lib/supabaseClient";
+import CustomModal from "../CustomModal";
+import { useState } from "react";
+
 export default function BlogPostCard(prop: {
   title: string;
   desc: string;
   index: number;
   date: string;
 }) {
+  const [open, setOpen] = useState(false);
+  const { user } = useAuthContext();
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const formattedDate = prop.date.split("T")[0].split("-").reverse().join(".");
 
+  const deletePost = async () => {
+    const { data, error } = await supabase
+      .from("blog_posts")
+      .delete()
+      .match({ id: prop.index });
+    window.location.reload();
+  };
+
   return (
-    <div id={`${prop.index}`} className="p-4 my-4 rounded-lg shadow-lg bg-black/10" key={prop.index}>
+    <div
+      id={`${prop.index}`}
+      className="p-4 my-4 rounded-lg shadow-lg bg-black/10"
+    >
+      {user.email === "luuka.lindgren@gmail.com" && (
+        <div className="relative">
+          <button
+            className="absolute right-0 px-6 py-2 transition duration-500 shadow-xl bg-black/20 rounded-xl hover:bg-black/40 active:scale-95"
+            onClick={handleOpen}
+          >
+            X
+          </button>
+        </div>
+      )}
       <h2 className="font-semibold text-center">{prop.title}</h2>
       <h3 className="mb-4 text-sm italic text-center">{formattedDate}</h3>
       {prop.desc.split("\n").map((paragraph, index) => (
@@ -16,6 +52,12 @@ export default function BlogPostCard(prop: {
           <br />
         </p>
       ))}
+      <CustomModal
+        open={open}
+        close={handleClose}
+        clicked={deletePost}
+        text="Haluatko varmasti poistaa tämän?"
+      />
     </div>
   );
 }
--- src/components/Book/BookPostCard.tsx
+++ src/components/Book/BookPostCard.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useAuthContext } from "@/app/context";
+import { supabase } from "@/lib/supabaseClient";
+import CustomModal from "../CustomModal";
+import { useState } from "react";
 import { Rating } from "@mui/material";
 
 export default function BookPostCard(prop: {
@@ -7,12 +13,37 @@ export default function BookPostCard(prop: {
   author: string;
   rating: number;
 }) {
+  const [open, setOpen] = useState(false);
+  const { user } = useAuthContext();
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const deleteSummary = async () => {
+    const { data, error } = await supabase
+      .from("book_summaries")
+      .delete()
+      .match({ id: prop.index });
+    window.location.reload();
+  };
+
   return (
     <div
       id={`${prop.index}`}
       className="p-4 my-4 rounded-lg shadow-lg bg-black/10"
-      key={prop.index}
     >
+      {user.email === "luuka.lindgren@gmail.com" && (
+        <div className="relative">
+          <button
+            className="absolute right-0 px-6 py-2 transition duration-500 shadow-xl bg-black/20 rounded-xl hover:bg-black/40 active:scale-95"
+            onClick={handleOpen}
+          >
+            X
+          </button>
+        </div>
+      )}
       <h2 className="font-semibold text-center">{prop.title}</h2>
       <h3 className="mb-4 text-sm italic text-center">{prop.author}</h3>
       {prop.desc.split("\n").map((paragraph, index) => (
@@ -24,6 +55,12 @@ export default function BookPostCard(prop: {
       <div className="pt-4 text-center">
         <Rating defaultValue={prop.rating} precision={0.5} readOnly />
       </div>
+      <CustomModal
+        open={open}
+        close={handleClose}
+        clicked={deleteSummary}
+        text="Haluatko varmasti poistaa tämän?"
+      />
     </div>
   );
 }
