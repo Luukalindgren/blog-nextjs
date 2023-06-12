1c73add2286f6eea556792fe5109d9948b6f5873
Luuka Lindgren <luslin@utu.fi>
2023-06-12 13:18:17 +0300

    Added state and fixed type errors

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,26 @@
 import BlogPostCard from "@/components/Blog/BlogPostCard";
 import BlogPostWidget from "@/components/Blog/BlogPostWidget";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
+import { useEffect, useState } from "react";
+import type { Post } from "@/lib/types";
 
 export const revalidate = 0;
 
 export default async function Blog() {
-  const { data: posts } = await supabase.from("blog_posts").select("*");
+  const [posts, setPosts] = useState<Post[] | null>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await supabase.from("blog_posts").select("*");
+        if (!data) return;
+        setPosts(data as Post[]);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchData();
+  }, [supabase]);
 
   return (
     <main className="container min-h-screen px-10 mx-auto mb-8 font-mono">
--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,22 +1,35 @@
 "use client";
 
 import BookPostCard from "@/components/Book/BookPostCard";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 import BookPostWidget from "@/components/Book/BookPostWidget";
+import { useEffect, useState } from "react";
+import type { Book } from "@/lib/types";
 
 export const revalidate = 0;
 
 export default async function Books() {
-  const { data: bookSummaries } = await supabase
-    .from("book_summaries")
-    .select("*");
+  const [bookSummaries, setBookSummaries] = useState<Book[] | null>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await supabase.from("book_summaries").select("*");
+        if (!data) return;
+        setBookSummaries(data as Book[]);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchData();
+  }, [supabase]);
 
   return (
     <main className="container min-h-screen px-10 mx-auto mb-8 font-mono">
       <h2 className="py-6 text-lg font-bold">Kirjatiivistelmät:</h2>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {!bookSummaries
+          {bookSummaries === null
             ? ""
             : bookSummaries.map((post) => (
                 <BookPostCard
