0e37fdb31c46e8870ea071ec5f8435b84e9c8229
Luuka Lindgren <luslin@utu.fi>
2023-06-12 13:18:41 +0300

    Refined types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,36 +1,37 @@
 export type Post = {
-    id: number;
-    title: string;
-    post: string;
-    inserted_at: string;
-  };
-  
-export type Book = {
-    id: number;
-    title: string;
-    author: string;
-    summary: string;
-    rating: number;
-  };
-
-export type User = {
-    app_metadata: {
-      provider: string;
-      providers: string[];
-    };
-    aud: string;
-    confirmation_sent_at: string;
-    confirmed_at: string;
-    created_at: string;
-    email: string;
-    email_confirmed_at: string;
-    id: string;
-    identities: [{}];
-    last_sign_in_at: string;
-    phone: string;
-    role: string;
-    updated_at: string;
-    user_metadata: {
-    };
+  id: number;
+  inserted_at: string;
+  post: string;
+  title: string;
+  updated_at: string;
 };
 
+export type Book = {
+  author: string;
+  id: number;
+  inserted_at: string;
+  rating: number;
+  summary: string;
+  title: string;
+  updated_at: string;
+};
+
+export type User = {
+  app_metadata: {
+    provider: string;
+    providers: string[];
+  };
+  aud: string;
+  confirmation_sent_at: string;
+  confirmed_at: string;
+  created_at: string;
+  email: string;
+  email_confirmed_at: string;
+  id: string;
+  identities: [{}];
+  last_sign_in_at: string;
+  phone: string;
+  role: string;
+  updated_at: string;
+  user_metadata: {};
+};
